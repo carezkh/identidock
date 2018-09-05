62260237dd8f5f8f18f7083ce3a7837c28258d4b
carezkh <2010267516@qq.com>
2018-09-05 18:57:49 +0800

    4 fix app/identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -39,7 +39,7 @@ def get_identicon(name):
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
-        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        r = requests.get('http://172.17.0.3:8080/monster/' + name + '?size=80')
         image = r.content
         cache.set(name, image)
 
